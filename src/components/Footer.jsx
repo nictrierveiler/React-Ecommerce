eed655d5f306c93f21ef9d4678ca59b2ad0d6c59
nictrierveiler <nicolastrierveiler@gmail.com>
2025-05-28 20:28:03 -0300

    FIX - Tailwind Config and Downgrade

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <div>
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             <div>
-                <img src={assets.logo} className='mb-5 w-18' alt="Logo" />
+                <img src={assets.logo} className='mb-5 w-16' alt="Logo" />
                 <p className='w-full md:w-2/3 text-gray-600'>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quos culpa tenetur officia voluptatibus voluptate!
                 </p>
