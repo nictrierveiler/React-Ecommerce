ec37138632bb393234936fb0eb9156115a043dea
nictrierveiler <nicolastrierveiler@gmail.com>
2025-06-13 02:33:58 -0300

    .env fix

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,6 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 
+    mongoose.connection.on('connected',() => {
+        console.log('DB Connected')
+    })
+
+    await mongoose.connect(`${process.env.MONGODB}/LaVeilleuse-Ecommerce`)
+
 }
 
 export default connectDB;
